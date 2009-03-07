fa7bdad8e6953a16677ba7d5039557794044975a
Justin Ball <justinball@gmail.com>
2009-03-07 13:56:51 -0700

    fixed theme_stylesheet_link_tag helper to accept all the same options as stylesheet_link_tag

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -1,7 +1,9 @@
 module ThemeHelper
 
-  def theme_stylesheet_link_tag
-    stylesheet_link_tag "themes/#{current_theme}/styles"
+  def theme_stylesheet_link_tag(*sources)
+    options = sources.extract_options!.stringify_keys
+    sheets = sources.collect {|style| "themes/#{current_theme}/#{style}"} || "themes/#{current_theme}/styles"
+    stylesheet_link_tag sheets, options
   end
 
 end
