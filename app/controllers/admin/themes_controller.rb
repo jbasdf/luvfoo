2df606fcca177bdea12e70a190fed0dff0c54cf3
Justin Ball <justinball@gmail.com>
2009-03-10 17:33:18 -0600

    refactored themes to push code into the model

--- app/controllers/admin/themes_controller.rb
+++ app/controllers/admin/themes_controller.rb
@@ -7,7 +7,7 @@ class Admin::ThemesController < Admin::BaseController
   cache_sweeper :site_sweeper, :only => [:update]
 
   def edit
-    get_themes
+    @current_theme, @themes = Site.available_themes(@site)
     respond_to do |format|
       format.html
     end
@@ -24,32 +24,6 @@ class Admin::ThemesController < Admin::BaseController
   end
 
   private
-
-  def get_themes
-    @themes = []
-    theme_path = File.join(RAILS_ROOT, 'themes')
-    Dir.glob("#{theme_path}/*").each do |theme_directory|
-      if File.directory?(theme_directory)
-        theme_name = File.basename(theme_directory)
-
-        image = Dir.glob(File.join(RAILS_ROOT, 'public', 'images', theme_name, 'preview.*')).first || File.join('/', 'images', 'no_preview.gif')
-        image = image.gsub(File.join(RAILS_ROOT, 'public'), '')
-
-        description = ''
-        description_file = File.join(theme_directory, 'description.txt')
-        if File.exist?(description_file)
-          f = File.new(description_file, "r")
-          description = f.read
-          f.close
-        end
-
-        theme = {:name => theme_name, :preview_image => image, :description => description}
-        @themes << theme
-        @current_theme = theme if @site.theme == theme_name
-
-      end
-	end
-  end
 
   def get_site
     @site = Site.first
