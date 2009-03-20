a760488c681f5d477a1d0649f083cfe98006f4f4
Justin Ball <justinball@gmail.com>
2009-03-20 14:34:42 -0600

    fixed environment.rb.  Cleaned test

--- test/unit/country_test.rb
+++ test/unit/country_test.rb
@@ -1,11 +1,5 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class CountryTest < ActiveSupport::TestCase
-    
-    should_have_many :users
-    
-    def test_associations
-        _test_associations
-    end
-    
+class CountryTest < ActiveSupport::TestCase  
+  should_have_many :users
 end
